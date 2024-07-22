aa0749f30cb2ab1e9d9455f3e518d6d60eca3ace
Tinu  kumar <tinu01edu@gmail.com>
2024-07-22 21:35:27 +0530

    first commit

---
+++ even.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class even{
+    public static void main(String[] arg){
+        Scanner sc =new Scanner(System.in);
+         int n=sc.nextInt();
+        if((n%2)==05)
+        {
+        System.out.println("this number is even");
+       } 
+       else {
+        System.out.println("this  is odd number");
+       }
+    
+    }
+
+}
---
+++ swap.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main( String[] args){
+ Scanner sc =new Scanner(System.in); 
+ System.out.println("enter the number");
+ int n;
+ n=sc.nextInt();
+         // int  n;
+         //  n=sc.nextInt();
+         //  int arr[];
+         //  arr=new int[n]; 
+         //  for (int i=0; i<n; i++){
+         //    System .out. println("enter the integer values:-");
+         //    arr[i]=sc.nextInt();
+         // }
+         // int max=0;
+         // for(int i=0; i<n; i++){
+         //    if(arr[i]>max)
+         //    max=arr[i];
+         // }
+
+         // System.out.println("the highest values throught out the interger is:- " + max);
+        
+System.out.println(int.MAXVALUE);
+
+         }   
+
+     }
+    
+
+    
+    
+
